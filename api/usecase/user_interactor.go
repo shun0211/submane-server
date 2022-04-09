d2c3177bc86721aece851c8f65fb2956100783a5
shun0211 <ss211_sabcabc12312395_18@yahoo.co.jp>
2022-04-09 14:12:13 +0900

    usecaseのpackage完了

--- api/usecase/user_interactor.go
+++ api/usecase/user_interactor.go
@@ -1,1 +1,36 @@
 package usecase
+
+import (
+	"./../domain"
+)
+
+type UserInteractor struct {
+	UserRepository UserRepository
+}
+
+
+func (interactor *UserInteractor) UserById(id int) (user domain.User, err error) {
+	// Note: 変数の初期化は戻り値に指定した変数でされているのでここでは=を使っている
+	user, err = interactor.UserRepository.FindById(id)
+	return
+}
+
+func (interactor *UserInteractor) Users() (users domain.Users, err error) {
+	users, err = interactor.UserRepository.FindAll()
+	return
+}
+
+func (interactor *UserInteractor) Add(u domain.User) (user domain.User, err error) {
+	user, err = interactor.UserRepository.Store(u)
+	return
+}
+
+func (interactor *UserInteractor) Update(u domain.User) (user domain.User, err error) {
+	user, err = interactor.UserRepository.Update(domain.User(u))
+	return
+}
+
+func (interactor *UserInteractor) DeleteById(u domain.User) (user domain.User, err error) {
+	user, err = interactor.UserRepository.DeleteById(domain.User(u))
+	return
+}
--- api/usecase/user_repository.go
+++ api/usecase/user_repository.go
@@ -4,4 +4,8 @@ import "submane-server/api/domain"
 
 type UserRepository interface {
 	FindById(id int) (domain.User, error)
+	FindAll() (domain.Users, error)
+	Store(domain.User) (domain.User, error)
+	Update(domain.User) (domain.User, error)
+	DeleteById(domain.User) error
 }
