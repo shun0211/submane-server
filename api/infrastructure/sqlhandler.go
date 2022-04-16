5721ba077c1a1322bca90a6c5d426c5169ea1800
shun0211 <ss211_sabcabc12312395_18@yahoo.co.jp>
2022-04-16 09:52:53 +0900

    around database

---
+++ api/infrastructure/sqlhandler.go
@@ -0,0 +1,59 @@
+package infrastructure
+
+import (
+	"submane-server/api/interfaces/controllers/database"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+type SqlHandler struct {
+	Conn *gorm.DB
+}
+
+func NewSqlHandler() database.SqlHandler {
+	dsn := "host=postgres user=root password=password dbname=sabumane_db port=5432 sslmode=disable TimeZone=Asia/Tokyo"
+	conn, err := gorm.Open(postgres.Open(dsn), &gorm.Config{}) // &gormはポインタ型
+	if err != nil {
+		panic(err.Error())
+	}
+	// FUCK: なぜか↓だとコンパイルエラーになる
+	// sqlHandler := SqlHandler{Conn: conn}
+	sqlHandler := new(SqlHandler)
+	sqlHandler.Conn = conn
+	return sqlHandler
+}
+
+func (handler *SqlHandler) Find(out interface{}, where ...interface{}) *gorm.DB {
+	return handler.Conn.Find(out, where...)
+}
+
+func(handler *SqlHandler) First(out interface{}, where ...interface{}) *gorm.DB {
+	return handler.Conn.First(out, where...)
+}
+
+// NOTE: SELECT文以外はExec関数を使う
+func(handler *SqlHandler) Exec(sql string, values ...interface{}) *gorm.DB {
+	return handler.Conn.Exec(sql, values...)
+}
+
+// NOTE: SELECT文はRaw関数を使う
+func(handler *SqlHandler) Raw(sql string, values ...interface{}) *gorm.DB {
+	return handler.Conn.Raw(sql, values...)
+}
+
+func(handler *SqlHandler) Create(value interface{}) *gorm.DB {
+	return handler.Conn.Create(value)
+}
+
+func(handler *SqlHandler) Save(value interface{}) *gorm.DB {
+	return handler.Conn.Save(value)
+}
+
+func(handler *SqlHandler) Delete(value interface{}) *gorm.DB {
+	return handler.Conn.Delete(value)
+}
+
+func(handler *SqlHandler) Where(query interface{}, args ...interface{}) *gorm.DB {
+	return handler.Conn.Where(query, args...)
+}
---
+++ api/interfaces/controllers/database/sqlhandler.go
@@ -0,0 +1,14 @@
+package database
+
+import "gorm.io/gorm"
+
+type SqlHandler interface {
+	Find(interface{}, ...interface{}) *gorm.DB
+	First(interface{}, ...interface{}) *gorm.DB
+	Exec(string, ...interface{}) *gorm.DB
+	Raw(string, ...interface{}) *gorm.DB
+	Create(interface{}) *gorm.DB
+	Save(interface{}) *gorm.DB
+	Delete(interface{}) *gorm.DB
+	Where(interface{}, ...interface{}) *gorm.DB
+}
