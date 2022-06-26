e4b97fc03da93821ea83a70d7d8cac944019aee3
shun0211 <ss211_sabcabc12312395_18@yahoo.co.jp>
2022-06-27 07:20:07 +0900

    ✏️ 環境変数の読み込みのための分岐作成

--- api/server.go
+++ api/server.go
@@ -9,11 +9,12 @@ import (
 )
 
 func main() {
-	// NOTE: GO_ENVで分岐を作る
-	os.Setenv("GOOGLE_APPLICATION_CREDENTIALS", "credentials/submane-firebase-adminsdk.json")
-	err := godotenv.Load()
-  if err != nil {
-    log.Fatal("Error loading .env file")
-  }
+	if os.Getenv("GO_ENV") == "development" {
+		os.Setenv("GOOGLE_APPLICATION_CREDENTIALS", "credentials/submane-firebase-adminsdk.json")
+		err := godotenv.Load()
+		if err != nil {
+			log.Fatal("Error loading .env file")
+		}
+	}
 	driver.Init()
 }
